2065936e187850c2b1fb1d1bac991b15cb114843
triveninelakuditi <42571671+triveninelakuditi@users.noreply.github.com>
2018-08-30 16:47:20 +0530

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,21 @@
+# your code goes here
+a=5
+b=1
+c=5
+d=2
+e=5
+f=3
+g=5
+i=4
+j=5
+k=5
+mul = a*b
+print(mul)
+mul = c*d
+print(mul)
+mul = e*f
+print(mul)
+mul = g*i
+print(mul)
+mul = j*k
+print(mul)
