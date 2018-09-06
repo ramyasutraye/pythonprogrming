8accd8f7f8b804bb3b4ae6e3525491cafe4bf402
triveninelakuditi <42571671+triveninelakuditi@users.noreply.github.com>
2018-09-06 12:05:47 +0530

    Create sorted.py

---
+++ sorted.py
@@ -0,0 +1,8 @@
+arr= [2,1,3]
+for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+        	if(arr[i]>arr[j]):
+        	    a= arr[i]
+        	    arr[i]= arr[j]
+        	    arr[j]= a
+print(arr)        	    
